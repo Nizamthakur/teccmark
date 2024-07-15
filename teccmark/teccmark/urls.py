9ba98463232b8b1bb243fa61e61c12126e8aa615
saklain nizam thakur <54598229+Nizamthakur@users.noreply.github.com>
2024-07-16 03:49:35 +0600

    UPDATED TOPBAR

--- teccmark/teccmark/urls.py
+++ teccmark/teccmark/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.homePage, name='homePage'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
